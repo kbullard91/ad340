1609da54a7e99302511b51bf54c7c3013fe37dbd
Kari Bullard <kari.e.bullard@gmail.com>
2017-05-01 21:24:02 -0700

    Homework 4

--- app/src/main/java/karibullard/com/ad340App/ButtonAdapter.java
+++ app/src/main/java/karibullard/com/ad340App/ButtonAdapter.java
@@ -1,8 +1,11 @@
 package karibullard.com.ad340App;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,8 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by karibullard on 4/26/17.
@@ -21,12 +26,13 @@ public class ButtonAdapter extends BaseAdapter {
     Button buttons[];
     LayoutInflater inflter;
 
+    String[] dialogItems = {"I like Lebron.", "I like Durant", "I like Curry", "I like turtles."};
+
     public ButtonAdapter(Context applicationContext, Button[] buttons) {
         this.context = applicationContext;
         this.buttons = buttons;
         inflter = (LayoutInflater.from(applicationContext));
     }
-
 
     @Override
     public int getCount() {
@@ -56,6 +62,46 @@ public class ButtonAdapter extends BaseAdapter {
                 if(position == 0){
                     Intent intent = new Intent(context, DisplayInfoList.class);
                     context.startActivity(intent);
+                } else if (position == 1) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setIcon(android.R.drawable.sym_def_app_icon);
+                    builder.setCancelable(false);
+                    builder.setItems(dialogItems, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int item) {
+                            switch (item) {
+                                case 0:
+                                    Log.d(TAG, "You like Lebron.");
+                                    break;
+                                case 1:
+                                    Log.d(TAG, "You like Durant.");
+                                    break;
+                                case 2:
+                                    Log.d(TAG, "You like Curry.");
+                                    break;
+                                case 3:
+                                    Log.d(TAG, "You like Turtles.");
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    });
+                    builder.setTitle("Pick your Favorite");
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                            Log.d(TAG, "OK was clicked.");
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                            Log.d(TAG, "Cancel was clicked.");
+                        }
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+
                 } else {
                     Toast.makeText(context, "" + position,
                             Toast.LENGTH_SHORT).show();
